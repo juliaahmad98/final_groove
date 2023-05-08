7b7e0e071651b7b9496f513b601071ee88fbb04c
juliaahmad98 <124204992+juliaahmad98@users.noreply.github.com>
2023-05-07 23:50:56 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,134 @@
+<?php 
+
+include ("connect.php");
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>The Groove Theory</title>
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=Montserrat:wght@100;200;300;400;500;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+
+
+</head>
+
+<body>
+
+
+    <div class="sidebar">
+        <div class="logo">
+            <a href="index.php">
+            <img src="./groovetheory.png" alt="Logo"/></a>
+        </div>
+  
+
+    <div class="navigation">
+
+        <ul>
+            <li>
+                <a href="index.php">
+                    <span class="fa fa-home"></span>
+                    <span>Home</span> </a>
+            </li>
+
+            <li>
+                <a href="upload.php">
+                    <span class="fa fas fa-plus"></span>
+                    <span>Groove With Me</span> </a>
+            </li>
+
+            <li>
+                <a href="profile.php">
+                    <span class="fa fa-user"></span>
+                    <span>Profile</span> </a>
+            </li>   
+
+        </ul>
+    </div>
+
+
+<div class="navigation">
+        <ul>
+
+
+            <li>
+                <a href="#">
+                    <span class="fa fa-headphones"></span>
+                    <span>Genres</span> </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <span class="fa fa-fingerprint"></span>
+                    <span>Artists</span> </a>
+            </li>
+
+             <li>
+                <a href="#">
+                    <span class="fa fa-record-vinyl"></span>
+                    <span>Record Labels</span> </a>
+            </li>
+
+            
+        </ul>
+    </div>
+
+    <div class="policies">
+        <ul>
+            <li>
+                <a href="#">Cookies</a>
+
+            </li>
+            <li>
+                <a href="#">Privacy</a>
+            </li>
+        </ul>
+</div>
+</div>
+
+
+
+<div class="main-container">
+<div class="topbar">
+    <div class="prev-next-buttons">
+        <button type="button" class="fa fas fa-chevron-left"></button>
+        <button type="button" class="fa fas fa-chevron-right"></button>
+</div>
+
+    <div class="navbar">
+        <ul>
+            <li>
+                <a href="about.php">About</a>
+            </li>
+             <li>
+                <a href="contactus.php">Contact</a>
+            </li>
+            <li class="divider">|</li>
+            <li>
+                <a href="register.php">Sign Up</a>
+            </li>   
+        </ul>
+        <a href="login.php"><button type="button">Log In</button></a>
+
+    </div>
+    </div>
+
+
+  
+    <script 
+      src="https://kit.fontawesome.com/23cecef777.js" 
+      crossorigin="anonymous">
+
+
+    </script>
+    
+</body>
+
+        
+
+
